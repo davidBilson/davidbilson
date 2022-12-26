2a4df19fb477829fd7ef9a66fa29f91498e52da5
davidBilson <davidbilson99@gmail.com>
2022-12-26 07:17:25 +0100

    happy boxing day

--- src/WebPortfolio/PortBoxes.jsx
+++ src/WebPortfolio/PortBoxes.jsx
@@ -7,6 +7,7 @@ import ImageFive from '../Assets/image5.jpeg';
 import AOS from "aos";
 import "aos/dist/aos.css";
 
+
 const PortBoxes = () => {
     useEffect(() => {
         AOS.init({duration:2000})
