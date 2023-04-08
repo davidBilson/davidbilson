b432748274ad0af7ff1002cccf8baf3cbfb11b93
davidBilson <davidbilson99@gmail.com>
2023-04-08 16:48:56 +0100

    updated SEO and google analytics

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Intro.css'
 import {FiLinkedin, FiTwitter, FiGithub} from 'react-icons/fi'
-
 const Intro = () => {
   return (
     <section className='intro'>
@@ -9,13 +8,13 @@ const Intro = () => {
         <h1 className='intro-text'>Hi, I am <span className="red">David Bilson</span></h1>
         <p className='intro-preface'>I'm a software engineer, I specialize in creating intuitive and user-friendly websites that help businesses and individuals achieve their goals online. </p>
         <span className='icon-container'>
-        <a href="" className='icons'>
+        <a href="https://twitter.com/David_Bilsonn" target="_blank" className='icons'>
         <FiTwitter/>
         </a>
-        <a href="https://github.com/davidBilson" className='icons'>
+        <a href="https://github.com/davidBilson" target="_blank" className='icons'>
         <FiGithub />
         </a>
-        <a href="https://www.linkedin.com/in/david-bilson-2401981a5/" className='icons'>
+        <a href="https://www.linkedin.com/in/david-bilson-2401981a5/" target="_blank" className='icons'>
         <FiLinkedin />
         </a>
         </span>
