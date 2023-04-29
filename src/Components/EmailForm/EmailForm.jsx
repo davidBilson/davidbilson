89228ead91c8546fdb42c3bf1dfed4d17a5d1603
davidBilson <davidbilson99@gmail.com>
2023-04-29 02:01:44 +0100

    form updated again

--- src/Components/EmailForm/EmailForm.jsx
+++ src/Components/EmailForm/EmailForm.jsx
@@ -1,19 +1,12 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import './EmailForm.css';
 import { MdOutlineSend } from 'react-icons/md';
+
 const EmailForm = () => {
-  const errorMessageStyle = {
-    color: "yellow",
-    fontSize: "12px",
-    fontWeight: "bold",
-    marginTop: "-12px",
-  }
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
-
-    setErrorMessage('');
     emailjs.sendForm('service_g3ea0qx', 'template_qfzfdfp', form.current, '0p5ukCIa4FZJtmdSE')
       .then((result) => {
           console.log(result.text);
@@ -25,14 +18,13 @@ const EmailForm = () => {
   };
   return (
     <section id='emailForm'>
-      <form ref={form} onSubmit={sendEmail} >
-        <input name='user_email' type="email" id="email" placeholder='Email' required />
-        <br />
-        <textarea name='user_message' id="message" placeholder='Write message...' required >
-        </textarea>
-        <br />
-        <button type="submit" id='submitBtn'>Shoot <MdOutlineSend /></button>
-      </form>
+    <form ref={form} onSubmit={sendEmail} >
+      <input name='user_email' type="email" id="email" placeholder='Email' required />
+      <br />
+      <textarea name='user_message' id="message" placeholder='Write message...' required ></textarea>
+      <br />
+      <button type="submit" id='submitBtn'>Shoot <MdOutlineSend /></button>
+    </form>
     </section>
   );
 };
