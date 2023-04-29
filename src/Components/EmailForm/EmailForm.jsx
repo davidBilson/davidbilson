01236c3b89c560592373beee14b766ece985ea69
davidBilson <davidbilson99@gmail.com>
2023-04-29 01:51:29 +0100

    Email updaed - regex removed

--- src/Components/EmailForm/EmailForm.jsx
+++ src/Components/EmailForm/EmailForm.jsx
@@ -10,15 +10,9 @@ const EmailForm = () => {
     marginTop: "-12px",
   }
   const form = useRef();
-  const [errorMessage, setErrorMessage] = useState('');
   const sendEmail = (e) => {
     e.preventDefault();
-    const message = form.current.user_message.value.trim();
-    const wordCount = message.split(/\s+/).length;
-    if (wordCount < 5) {
-      setErrorMessage("Come on!! It's less than 5 words...");
-      return;
-    }
+
     setErrorMessage('');
     emailjs.sendForm('service_g3ea0qx', 'template_qfzfdfp', form.current, '0p5ukCIa4FZJtmdSE')
       .then((result) => {
@@ -36,7 +30,6 @@ const EmailForm = () => {
         <br />
         <textarea name='user_message' id="message" placeholder='Write message...' required >
         </textarea>
-        {errorMessage && <p style={errorMessageStyle}>{errorMessage}</p>}
         <br />
         <button type="submit" id='submitBtn'>Shoot <MdOutlineSend /></button>
       </form>
