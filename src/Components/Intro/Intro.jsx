c3c495bcae8bce0f97d351f4df2e5b5f7c09087b
davidBilson <davidbilson99@gmail.com>
2023-11-05 17:46:30 +0100

    update

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -7,7 +7,7 @@ const Intro = () => {
     <section className='intro'>
       <div className="intro-container">
         <h1 className='intro-text'>Software<span className="red"> Developer</span></h1>
-        <p className='intro-preface'>Hi, I'm David Bilson. I am a Fullstack Web Developer, my specialization revolves around developing intuitive and user-friendly websites that enable businesses and individuals to effectively accomplish their objectives.</p>
+        <p className='intro-preface'>Hi, I'm David Bilson. I am a Fullstack Software Developer, my specialization revolves around developing intuitive and user-friendly websites that enable businesses and individuals to effectively accomplish their objectives.</p>
         <span className='icon-container'>
         <a href="https://twitter.com/David_Bilsonn" target="_blank" className='icons'>
         <FiTwitter/>
